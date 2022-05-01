69138f832f503d1beda87834f62a3d7ffc0a7a3e
Shubham Nhalde <96485743+ShubhamNhalde@users.noreply.github.com>
2022-05-02 01:27:41 +0530

    Update: Ability for the Services Layer to Store the AddressBook data

--- src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
@@ -1,32 +1,36 @@
-package com.bridgelabz.addressbook.model;
+package com.bridgelabz.addressbook2.model;
 
+import com.bridgelabz.addressbook2.dto.AddressBookDTO;
+import lombok.Data;
 
-import com.bridgelabz.addressbook.dto.AddressBookDTO;
-import lombok.Getter;
-import lombok.Setter;
+public @Data class AddressBookData {
 
-@Getter
-@Setter
-public class AddressBookData {
-
-    private int id;
-    private String firstName;
-    private String lastName;
+    private int person_id;
+    private String first_name;
+    private String last_name;
     private String address;
+    private String phone_no;
     private String city;
     private String state;
-    private String zipcode;
-    private long mobile_no;
+    private String zip_code;
 
 
-    public AddressBookData(int id, AddressBookDTO addressBookDTO) {
-        this.id = id;
-        this.firstName = addressBookDTO.firstName;
-        this.lastName = addressBookDTO.lastName;
+
+    public AddressBookData(int i, AddressBookDTO addressBookDTO) {
+        this.person_id = i;
+        this.updateAddressBookData(addressBookDTO);
+    }
+
+    public void updateAddressBookData(AddressBookDTO addressBookDTO) {
+
+        this.first_name = addressBookDTO.first_name;
+        this.last_name = addressBookDTO.last_name;
         this.address = addressBookDTO.address;
+        this.phone_no = addressBookDTO.phone_no;
         this.city = addressBookDTO.city;
         this.state = addressBookDTO.state;
-        this.zipcode = addressBookDTO.zipcode;
-        this.mobile_no = addressBookDTO.mobile_no;
+        this.zip_code = addressBookDTO.zip_code;
+
     }
 }
+
